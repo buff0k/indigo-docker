0d18f27719c883f5db5bc90206cba98abaf5c0e2
Eben van Deventer <buff0k@gmail.com>
2021-09-15 16:52:24 +0200

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd martor_demo; python manage.py createsuperuser --no-input)
 fi
-(cd indigo; gunicorn martor_demo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) $
+(cd indigo; gunicorn indigo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) $
 nginx -g "daemon off;"
