de9a32019652bd59062e3a7cb52a18a1e7b6517a
Eben van Deventer <buff0k@gmail.com>
2021-11-25 16:16:53 +0200

    Update entrypoint.sh

--- indigo/entrypoint.sh
+++ indigo/entrypoint.sh
@@ -11,7 +11,9 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
+python manage.py makemigrations 
 python manage.py migrate
+python manage.py award_badges
 
 exec "$@"
